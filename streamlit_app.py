e1d497ef6ddc83f5b7720e50de2a22d4c5c9afd3
Udaybk01 <112836149+Udaybk01@users.noreply.github.com>
2022-09-05 21:59:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 import snowflake.connector
 import pandas
+import requests
+from urllib.error import URLError
+
 streamlit.title('My Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
@@ -14,7 +17,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-import requests
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
